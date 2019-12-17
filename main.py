7b2f9570957b60c5ab70068f7e9f9393e32458ba
viktorshv <viktor.shv1995@gmail.com>
2019-12-17 21:57:40 +0700

    Add exceptions

--- main.py
+++ main.py
@@ -17,21 +17,25 @@ def download_comics_picture(picture_id):
     picture_name = f'{picture_id}.png'
     with open(picture_name, 'wb') as image:
         image.write(response.content)
+ 
+def check_vk_api_error(response):
+    if 'error' in response:
+        raise requests.exceptions.HTTPError(response['error']['error_msg'])
 
-def get_upload_information(group_id):
+def get_upload_information(group_id, access_token, version):
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     params = {
         'access_token': access_token,
         'group_id': group_id,
-        'v': 5.103
+        'v': version
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
-
+    check_vk_api_error(response.json())
     return response.json()['response']['upload_url']
 
 def upload_comics(picture_id, group_id):
-    upload_url = get_upload_information(group_id)
+    upload_url = get_upload_information(group_id, access_token, version)
     download_comics_picture(picture_id)
     picture_file = open(f'{picture_id}.png', 'rb')
     files = {'photo': picture_file}
@@ -39,38 +43,41 @@ def upload_comics(picture_id, group_id):
     response.raise_for_status()
     response_data = response.json()
     picture_file.close()
+    check_vk_api_error(response_data)
     
     return response_data['server'], response_data['photo'], response_data['hash']
 
-
-def get_post_information(picture_id, group_id):
+def get_post_information(picture_id, group_id, access_token, version):
     server, photo, _hash = upload_comics(picture_id, group_id)
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     params = {
        'access_token': access_token,
        'group_id': group_id,
-       'v': 5.103,
+       'v': version,
        'server': server,
        'photo': photo, 
        'hash': _hash
     }
     response = requests.post(url, params=params).json()
+    check_vk_api_error(response)
 
     return response['response'][0]['id'], response['response'][0]['owner_id']
 
-def post_picture(picture_id, group_id):
-    media_id, owner_id = get_post_information(picture_id, group_id)
+def post_picture(picture_id, group_id, access_token, version):
+    media_id, owner_id = get_post_information(picture_id, group_id, access_token, version)
     url = 'https://api.vk.com/method/wall.post'
     params = {
         'access_token': access_token,
-        'v': 5.103,
+        'v': version,
         'owner_id': f'-{group_id}',
         'message': get_comics_picture_info(picture_id)['alt'],
         'attachments': f'photo{owner_id}_{media_id}',
         'from_group': 1,
     }
     
-    requests.get(url, params=params).json()
+    response = requests.get(url, params=params).json()
+    check_vk_api_error(response)
+
     os.remove(f'{picture_id}.png')
 
 def get_last_comics_number():
@@ -83,7 +90,12 @@ def get_last_comics_number():
 if __name__ == "__main__":
     load_dotenv()
     access_token = os.getenv('ACCESS_TOKEN')
+    version = 5.103
     group_id = os.getenv('GROUP_ID')
-    
     random_comics = random.randint(1, get_last_comics_number())
-    post_picture(random_comics, group_id)
+    try:
+        post_picture(random_comics, group_id, access_token, version)
+    except Exception as e:
+        print(f'Ошибка: {e}')
+    finally:
+        os.remove(f'{random_comics}.png')
